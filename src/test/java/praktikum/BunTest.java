d300532b0321f0c1c8ea745ec0820772cc4187bd
alex_karepin <alex.karepin@traektoria.ru>
2022-02-09 15:12:54 +0300

    BunTest cleanup

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,44 +1,27 @@
 package praktikum;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
-@RunWith(MockitoJUnitRunner.class)
-
 public class BunTest {
 
-    @Mock
-    Bun bun;
-    String name;
-    float price;
+    Bun bun = new Bun("Name",123.0f);
 
     @Test
-    public void testGetName() {
-        Mockito.when(bun.getName()).thenReturn("Name");
+    public void getNameTest() {
+
         String actual = bun.getName();
         String expected = "Name";
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testGetName2() {
-        Bun bun = new Bun("Name",price);
-        String actual = bun.getName();
-        String expected = "Name";
-        assertEquals(expected, actual);
-    }
+    public void getPriceTest() {
 
-    @Test
-    public void testGetPrice() {
-        Mockito.when(bun.getPrice()).thenReturn(100f);
         float actual = bun.getPrice();
-        float expected = 100f;
-        assertEquals(expected, actual, 0);
+        float expected = 123.0f;
+        assertEquals(expected, actual,0.0);
     }
+
 }
